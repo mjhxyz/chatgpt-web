15b7720cec1b14f6579f19ff012eabba28f65073
Mao <mjhxyz@foxmail.com>
2023-03-20 21:48:38 +0800

    update: update something

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/48859684?v=4',
+      name: 'Mao',
+      description: 'Star on <a href="https://github.com/mjhxyz" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
